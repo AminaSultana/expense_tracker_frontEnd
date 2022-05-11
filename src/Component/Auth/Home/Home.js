ce4bf9e0a17e61ae937be5d850f865d5d8a5a5b2
AminaSultana <lilisultana53@gmail.com>
2022-05-11 11:44:45 +0530

    task 6

--- src/Component/Auth/Home/Home.js
+++ src/Component/Auth/Home/Home.js
@@ -1,22 +1,63 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import AuthContext from "../../../store/auth-context";
 
 import classes from "./Home.module.css";
 import UserProfileForm from "./UserProfileForm";
 
 const Home = () => {
   const [showForm, setShowForm] = useState(false);
-  const completeBtnHandler = () => {
+  const authCtx = useContext(AuthContext);
+  const navigate = useNavigate()
+
+  const completeBtnHandler = (event) => {
+    event.preventDefault();
     setShowForm(true);
   };
+
+  const verifyEmailHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCJxA_K5UfddM0iHg_NzvLXksyPx1KWeV4",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: authCtx.idToken,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        if(!response.ok){
+          const responseBody = response.json();
+          throw new Error(responseBody.error.message);
+          
+        }
+        alert("Email verified")
+
+    } catch (error) {
+      alert(error)
+    }
+    
+    
+  };
+
+  const logoutHandler = ()=>{
+    localStorage.removeItem("token")
+    authCtx.logout()
+    navigate("/");
+  }
 
   return (
     <div>
       <section className={classes.section}>
         <span>
-          <h1>
-            Welcome to expense tracker
-            </h1>
-            </span>
+          <h1>Welcome to expense tracker</h1>
+          <button onClick={logoutHandler}>Logout</button>
+        </span>
         <span className={classes.profile}>
           <span>Your profile is incomplete.</span>
           <button className={classes.profile_btn} onClick={completeBtnHandler}>
@@ -25,6 +66,9 @@ const Home = () => {
         </span>
       </section>
       <section>{showForm && <UserProfileForm />}</section>
+      <section>
+        <button onClick={verifyEmailHandler}>Verify Email</button>
+      </section>
     </div>
   );
 };
--- src/store/AuthContextProvider.js
+++ src/store/AuthContextProvider.js
@@ -2,15 +2,22 @@ import React, {useState} from 'react';
 import AuthContext from './auth-context';
 
 const AuthContextProvider = (props) => {
-    const [idToken, setIdToken] = useState();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const initialToken = localStorage.getItem("token")
+    const [idToken, setIdToken] = useState(initialToken);
+    const isLoggedIn = !!idToken;
     const loginHandler = (token)=>{
         setIdToken(token);
     }
+    const logoutHandler = ()=>{
+        setIdToken(null)
+        localStorage.removeItem("token")
+    }
+
     const contextValue={
         idToken: idToken,
         isLoggedIn: isLoggedIn,
         login: loginHandler,
+        logout: logoutHandler
     }
   return (
     <AuthContext.Provider value={contextValue}>
